4c7e89110eafe82e1873fcf71ddf9ec35759e644
Joel Tesfaye <joel@Joels-MacBook-Pro.local>
2022-12-17 11:45:42 +0300

    addidng full_name.cpp

---
+++ full_name.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    std::string s, y;
+    cout << "Write your full name.\n";
+    cin >> s >> y;
+    cout << s << " " << y;
+    return 0;
+}
